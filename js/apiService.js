2b30f351e64c1534c6c859ae7e1eca27800a0c9a
bupoo123 <bupooo@gmail.com>
2025-10-09 17:59:29 +0800

    回退到工作的API地址

--- js/apiService.js
+++ js/apiService.js
@@ -4,7 +4,7 @@
 
 class ApiService {
     constructor() {
-        this.baseURL = 'https://backend-h7zt70zb7-bupoos-projects.vercel.app/api';
+        this.baseURL = 'https://backend-4keycj5me-bupoos-projects.vercel.app/api';
         this.token = localStorage.getItem('authToken');
     }
 
