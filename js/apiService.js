418458864d0c1ce29da4589ffb9045c1d5652301
bupoo123 <bupooo@gmail.com>
2025-10-09 15:26:15 +0800

    🔧 更新API地址到最新部署
    - 更新前端API地址到修复后的后端
    - 确保登录功能正常工作

--- js/apiService.js
+++ js/apiService.js
@@ -4,7 +4,7 @@
 
 class ApiService {
     constructor() {
-        this.baseURL = 'https://backend-nnnlhkbs0-bupoos-projects.vercel.app/api';
+        this.baseURL = 'https://backend-lo879gbuj-bupoos-projects.vercel.app/api';
         this.token = localStorage.getItem('authToken');
     }
 
