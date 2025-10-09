bc6e50908e9abe9427acac0210fe3e9b0f8285c7
bupoo123 <bupooo@gmail.com>
2025-10-09 16:10:51 +0800

    更新API地址

--- js/apiService.js
+++ js/apiService.js
@@ -4,7 +4,7 @@
 
 class ApiService {
     constructor() {
-        this.baseURL = 'https://backend-jerkfn8z4-bupoos-projects.vercel.app/api';
+        this.baseURL = 'https://backend-67ajbven9-bupoos-projects.vercel.app/api';
         this.token = localStorage.getItem('authToken');
     }
 
